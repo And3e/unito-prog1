bcf13ec22b7e6b66e5bf7208d0a6e9437fe7b9eb
André <andre.marguerettaz@gmail.com>
2025-02-28 16:25:29 +0100

    introduzione malloc

---
+++ c/25-prog2-first/main.c
@@ -0,0 +1,31 @@
+#include "func.h"
+
+void es1() {
+  ciao();
+}
+
+int main() {
+  
+  
+  // cast-type *ptz;
+  // ptz = (cast-type *) malloc (byte-size)
+  // --> sizeof
+  
+  // malloc restituisce *void (fare poi il cast)
+
+  // int *x;
+  // x = (int *) malloc (sizeof (int));
+
+
+  // free(x);
+
+  // ---------------
+  
+  // struct
+  // unione
+  
+  // ---------------
+
+  // STACK - HEAP
+}
+
